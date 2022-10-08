18c06aa979ac46a220f48f398e390308458fd41c
legabi <legabi.dev@gmail.com>
2022-10-08 23:27:32 +0200

    Fix proxmox port

--- proxmox/prox-forprox.sh
+++ proxmox/prox-forprox.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # IP du proxmox
-PROXMOX_IP = "88.151.197.29"
+PROXMOX_IP=88.151.197.29
 
 # si il y a 3 arguments
 if [ $# -eq 3 ]
 then
 # on récupère les arguments
-    VM_IP = $1
-    DPORT = $2
-    SPORT = $3
+    VM_IP=$1
+    DPORT=$2
+    SPORT=$3
     # on crée les règle
     iptables -t nat -A PREROUTING -p tcp --dport $DPORT -j DNAT --to-destination $VM_IP:$SPORT
     iptables -t nat -A POSTROUTING -p tcp --sport $SPORT -s 10.1.18.12 -j SNAT --to-source $PROXMOX_IP:$DPORT
